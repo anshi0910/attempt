462e28e1bd2cadc79542cca69d3280815efc3d86
Anshika Bagla <anshikabagla@Anshikas-MacBook-Pro.local>
2017-12-14 10:27:01 -0800

    first commit

---
+++ firstthing.py
@@ -0,0 +1,14 @@
+print("hello world")
+# == to check if something is equal
+# != not equal
+# <, >, <=, >=
+if True:
+    print("aaron")
+if False:
+    print("aaah")
+
+f= input()
+if f == "clock":
+    print ("fan")
+else:
+    print ("TV")
